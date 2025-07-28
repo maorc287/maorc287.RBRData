e1ed97bed62fda75a18205cd466eb7a5df239e4f
maorc <m287@web.de>
2025-07-28 20:42:24 +0200

    Some new data added

--- RBRDataPlugin.cs
+++ RBRDataPlugin.cs
@@ -44,7 +44,14 @@ namespace maorc287.RBRDataExtPlugin
             PluginManager.AddProperty("RBR.EngineStatus", GetType(), 0, "");
             PluginManager.AddProperty("RBR.BatteryVoltage", GetType(), 0, "");
             PluginManager.AddProperty("RBR.BatteryStatus", GetType(), 0, "");
+            PluginManager.AddProperty("RBR.OilPressureWarning", GetType(), 0, "");
             PluginManager.AddProperty("RBR.LowBatteryWarning", GetType(), 0, "");
+            PluginManager.AddProperty("RBR.OilPumpDamage", GetType(), 1, "");
+            PluginManager.AddProperty("RBR.WaterPumpDamage", GetType(), 1, ""); 
+            PluginManager.AddProperty("RBR.ElectricSystemDamage", GetType(), 1, "");
+            PluginManager.AddProperty("RBR.BrakeCircuitDamage", GetType(), 1, "");
+            PluginManager.AddProperty("RBR.WheelLock", GetType(), 0, "");
+            PluginManager.AddProperty("RBR.WheelSlip", GetType(), 0, "");
         }
 
         public void End(PluginManager pluginManager) { }
@@ -62,7 +69,14 @@ namespace maorc287.RBRDataExtPlugin
             PluginManager.SetPropertyValue("RBR.EngineStatus", GetType(), snapshot.IsEngineOn);
             PluginManager.SetPropertyValue("RBR.BatteryVoltage", GetType(), snapshot.BatteryVoltage);
             PluginManager.SetPropertyValue("RBR.BatteryStatus", GetType(), snapshot.BatteryStatus);
+            PluginManager.SetPropertyValue("RBR.OilPressureWarning", GetType(), snapshot.OilPressureWarning);
             PluginManager.SetPropertyValue("RBR.LowBatteryWarning", GetType(), snapshot.LowBatteryWarning);
+            PluginManager.SetPropertyValue("RBR.OilPumpDamage", GetType(), snapshot.OilPumpDamage);
+            PluginManager.SetPropertyValue("RBR.WaterPumpDamage", GetType(), snapshot.WaterPumpDamage);
+            PluginManager.SetPropertyValue("RBR.ElectricSystemDamage", GetType(), snapshot.ElectricSystemDamage);
+            PluginManager.SetPropertyValue("RBR.BrakeCircuitDamage", GetType(), snapshot.BrakeCircuitDamage);
+            PluginManager.SetPropertyValue("RBR.WheelLock", GetType(), snapshot.WheelLock);
+            PluginManager.SetPropertyValue("RBR.WheelSlip", GetType(), snapshot.WheelSlip);
         }
 
         private uint GetProcessIdByName(string processName)
@@ -79,6 +93,62 @@ namespace maorc287.RBRDataExtPlugin
             float pressureRawBar = pressureRawPascal * 1e-5f;
             return pressureBase + pressureRawBar;
         }
+
+        float CalculateWheelLock(
+    float velocityX,
+    float velocityY,
+    float velocityZ,
+    float forwardX,
+    float forwardY,
+    float forwardZ,
+    float wheelSpeed)
+        {
+            // Compute car speed along forward direction
+            float carSpeed = (velocityX * forwardX +
+                              velocityY * forwardY +
+                              velocityZ * forwardZ) * -3.559f;
+
+            if (carSpeed < 1.0f)
+                return 0.0f;
+
+            // Compute lock ratio
+            float lockRatio = (carSpeed - wheelSpeed) / carSpeed;
+
+            // Clamp between 0.0 and 1.0
+            if (lockRatio < 0.0f) lockRatio = 0.0f;
+            if (lockRatio > 1.0f) lockRatio = 1.0f;
+
+            return lockRatio;
+        }
+
+        float CalculateWheelSlip(
+    float velocityX,
+    float velocityY,
+    float velocityZ,
+    float forwardX,
+    float forwardY,
+    float forwardZ,
+    float wheelSpeed)
+        {
+            // Compute car speed along forward direction
+            float carSpeed = (velocityX * forwardX +
+                              velocityY * forwardY +
+                              velocityZ * forwardZ) * -3.559f;
+
+            if (carSpeed < 1.0f)
+                return 0.0f;
+
+            // Compute spin ratio
+            float spinRatio = (wheelSpeed - carSpeed) / carSpeed;
+
+            // Clamp between 0.0 and 1.0
+            if (spinRatio < 0.0f) spinRatio = 0.0f;
+            if (spinRatio > 1.0f) spinRatio = 1.0f;
+
+            return spinRatio;
+        }
+
+
 
         private RBRData ReadRBRData()
         {
@@ -125,18 +195,50 @@ namespace maorc287.RBRDataExtPlugin
                     MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.OilPressureRawPascal));
                 snapshot.OilPressureBar = CalculateOilPressure(oilRawBase, oilRawPascal);
 
+                snapshot.OilPressureWarning = oilRawPascal < 0.8f;
+
                 // Battery status raw value
                 snapshot.BatteryStatus = 
-                    MemoryReader.ReadFloat(hProcess, new IntPtr(carInfoBase + Offsets.CarInfo.BatteryWear));
+                    MemoryReader.ReadFloat(hProcess, new IntPtr(carInfoBase + Offsets.CarInfo.BatteryStatus));
 
                 // Battery Voltage Calculation
                 snapshot.BatteryVoltage = snapshot.IsEngineOn
                     ? 14.5f
-                    : (snapshot.BatteryStatus / 12) * 2.4f + 10.4f;
+                    : (snapshot.BatteryStatus * 0.2f) + 10.4f;
 
                 // Low Battery Warning 
                 snapshot.LowBatteryWarning =
                    snapshot.BatteryStatus < 10.0f;
+
+                if (snapshot.IsEngineOn)
+                {
+                    float velocityX = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.VelocityX));
+                    float velocityY = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.VelocityY));
+                    float velocityZ = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.VelocityZ));
+                    float forwardX = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.ForwardX));
+                    float forwardY = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.ForwardY));
+                    float forwardZ = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarMov.ForwardZ));
+
+                    float wheelSpeed = MemoryReader.ReadFloat(hProcess, new IntPtr(carMovBase + Offsets.CarInfo.WheelSpeed));
+
+                    snapshot.WheelLock = 
+                        CalculateWheelLock(velocityX, velocityY, velocityZ, forwardX, forwardY, forwardZ, wheelSpeed);
+                    snapshot.WheelSlip = 
+                        CalculateWheelSlip(velocityX, velocityY, velocityZ, forwardX, forwardY, forwardZ, wheelSpeed);
+
+                }
+
+                // Read damage values
+                int damagePointer =
+                    MemoryReader.ReadInt(hProcess, new IntPtr(carMovBase + Offsets.CarMov.DamageStructurePointer));
+                snapshot.OilPumpDamage = 
+                    MemoryReader.ReadInt(hProcess, new IntPtr(damagePointer + Offsets.Damage.OilPump));
+                snapshot.WaterPumpDamage = 
+                    MemoryReader.ReadInt(hProcess, new IntPtr(damagePointer + Offsets.Damage.WaterPump));
+                snapshot.ElectricSystemDamage = 
+                    MemoryReader.ReadInt(hProcess, new IntPtr(damagePointer + Offsets.Damage.ElectricSystem));
+                snapshot.BrakeCircuitDamage = 
+                    MemoryReader.ReadInt(hProcess, new IntPtr(damagePointer + Offsets.Damage.BrakeCircuit));
 
             }
             catch (Exception ex)
@@ -164,11 +266,14 @@ namespace maorc287.RBRDataExtPlugin
         public float BatteryVoltage { get; set; } = 12.8f;
         public float BatteryStatus { get; set; } = 12.0f;
         public bool LowBatteryWarning { get; set; } = false;
+        public float WheelLock { get; set; } = 0.0f;
+        public float WheelSlip { get; set; } = 0.0f;
 
         // Damage values
         public int OilPumpDamage { get; set; } = 1;
         public int WaterPumpDamage { get; set; } = 1;
         public int ElectricSystemDamage { get; set; } = 1;
+        public int BrakeCircuitDamage { get; set; } = 1;
 
     }
 
@@ -176,9 +281,12 @@ namespace maorc287.RBRDataExtPlugin
     {
         public static class CarInfo
         {
-            public const int EngineStatus = 0x2B8;
-            public const int BatteryWear = 0x2B4;
+            public const int WheelSpeed = 0xC;
             public const int TurboPressurePascal = 0x18;
+            public const int WaterTemperatureCelsius = 0x14;
+            public const int EngineStatus = 0x2B8;
+            public const int BatteryStatus = 0x2B4;
+            public const int CoolantTemperatureKelvin = 0x1170;
         }
 
         public static class CarMov
@@ -186,6 +294,26 @@ namespace maorc287.RBRDataExtPlugin
             public const int OilPressureRawBase = 0x139C;
             public const int OilPressureRawPascal = 0x13AC;
             public const int OilTempKelvin = 0x138C;
+            public const int DamageStructurePointer = 0x620;
+
+            // Velocity vector components
+            public const int VelocityX = 0x120;
+            public const int VelocityY = 0x124;
+            public const int VelocityZ = 0x11C;
+
+            // Forward direction vector components
+            public const int ForwardX = 0x1C4;
+            public const int ForwardY = 0x1C8;
+            public const int ForwardZ = 0x1C0;
+        }
+
+        public static class Damage
+        {
+            public const int BatteryStatusPercent = 0x8C;
+            public const int OilPump = 0xE8;
+            public const int WaterPump = 0xDC;
+            public const int ElectricSystem = 0x1E8;
+            public const int BrakeCircuit = 0x80;
         }
 
         public static class Pointers
@@ -195,8 +323,6 @@ namespace maorc287.RBRDataExtPlugin
             public const int GameMode = 0x007EAC48;
             public const int GameModeOffset = 0x728;
 
-            public const int BatterySatusBasePointer1 = 0x0127EA70;
-            public static readonly int[] BatteryStatusChain = { 0x9C, 0x430, 0x1D0, 0x8C };
         }
     }
 }
